98988912a8b39e69c714e7d1e44d728454b95031
yusuffikri <yusuffiks99@gmail.com>
2023-02-27 23:42:46 +0800

    data journey update

--- src/sections/journey/data.js
+++ src/sections/journey/data.js
@@ -4,7 +4,7 @@ import { VscOrganization } from "react-icons/vsc";
 import { GiTeacher } from "react-icons/gi";
 
 const data = [
-  { id: 1, icon: <GiGraduateCap />, title: "State Polytechnic of Jember", desc: "I'm a Frontend Web Developer with a Assosiate degree in Computer Engineering at State Polytechnic of Jember from 2019 until 2022." },
+  { id: 1, icon: <GiGraduateCap />, title: "State Polytechnic of Jember", desc: "I'm a Frontend Web Developer with an Assosiate degree in Computer Engineering at State Polytechnic of Jember from 2019 until 2022." },
   { id: 2, icon: <HiLibrary />, title: "Central Goverment of Jember District", desc: "I was got opportunity an Internship Program at Central Goverment of Jember District for 4 month." },
   {
     id: 3,
